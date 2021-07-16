d96f08fdb9694f677cee84fbcf89b15d1a40736c
Sergey <derangeer@gmail.com>
2021-07-16 22:44:12 +0300

    added some files

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,10 @@
 import json
 
 def lambda_handler(event, context):
-    # TODO implement
+    print("123")
+    
+    print("123")
+    print("123")
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
